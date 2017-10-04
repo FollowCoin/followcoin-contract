f3b4c7ab05497659f7b32f8a8519cb0bbe0408cc
Tomasz <tbec22@gmail.com>
2017-10-04 12:12:41 +0200

    Gas limit

--- contracts/FollowCoinPreSale.sol
+++ contracts/FollowCoinPreSale.sol
@@ -314,7 +314,7 @@ contract FollowCoinPreSale is Haltable {
      *
      * The function without name is the default function that is called whenever anyone sends funds to a contract
      */
-    function () payable preSaleActive inNormalState {
+    function () payable preSaleActive inNormalState validGasPrice {
         require(msg.value > 0);
        
         uint amount = msg.value;
@@ -334,7 +334,7 @@ contract FollowCoinPreSale is Haltable {
 
         tokenReward.transfer(msg.sender, tokens);
         multisig.transfer(amount);
-        FundTransfer(msg.sender, amount, true);
+        //FundTransfer(msg.sender, amount, true);
     }
 
 
